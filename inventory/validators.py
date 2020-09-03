2629e96e39487919dfeaed29b928c889037c507d
sharonorengo <47815725+sharonorengo@users.noreply.github.com>
2020-09-03 08:58:11 +0200

    Update validators.py

--- inventory/validators.py
+++ inventory/validators.py
@@ -5,4 +5,3 @@ from django.utils.translation import ugettext_lazy as u
 def validate_digit_length(gtin):
     if not (len(str(gtin)) == 13): 
     	raise ValidationError(u"gtin must be 13 digits.",params={'gtin': gtin})
-    	#raise ValidationError(_"Sorry, the email submitted is invalid. All emails have to be registered on this domain only.", status='invalid')
